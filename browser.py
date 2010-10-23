cf0e90c870d3785ef43e94878f7aeb2fc9193f81
davemds <davemds@6314f7e4-a9b4-11dd-a77a-2d921c902ce3>
2010-10-23 23:00:51 +0000

    use show() instead of bring_in() when scrolling with keybord/lirc/joy

--- browser.py
+++ browser.py
@@ -403,14 +403,14 @@ class ViewList(object):
          next = item.next_get()
          if next:
             next.selected_set(1)
-            next.middle_bring_in()
+            next.show()
             return input.EVENT_BLOCK
 
       elif event == "UP":
          prev = item.prev_get()
          if prev:
             prev.selected_set(1)
-            prev.middle_bring_in()
+            prev.show()
             return input.EVENT_BLOCK
 
       elif event == "OK":

159ae6dbd718bb2a131b61b04f5aa069a7b2ade1
dave@gurumeditation.it <dave@gurumeditation.it@6314f7e4-a9b4-11dd-a77a-2d921c902ce3>
2013-02-03 12:29:11 +0000

    unicode simbol for (c) in credits

--- epymc/gui.py
+++ epymc/gui.py
@@ -639,7 +639,7 @@ MONOPOLIO DI STATO
 
 
 license
-Copyright (C) 2010-2013 Davide Andreoli <dave@gurumeditation.it>
+Copyright © 2010-2013 Davide Andreoli <dave@gurumeditation.it>
 
 EpyMC is free software: you can redistribute it and/or modify it under the terms of the GNU Lesser General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
 

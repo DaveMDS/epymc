3cdb7add20e78f2bc1bea6b25623d7f217f3927d
Dave Andreoli <dave@gurumeditation.it>
2015-07-13 19:33:02 +0200

    Prepare the 1.1.0-beta1 release
    also updated yt-download to the latest version

--- epymc/__init__.py
+++ epymc/__init__.py
@@ -19,4 +19,4 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 # follow semver.org specs (ex: '1.0.0' and '1.0.0-beta1')
-__version__ = '1.0.0'
+__version__ = '1.1.0-beta1'

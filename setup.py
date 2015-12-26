56e5197c9d62b96b1ba2bf400afc766e5660677f
Dave Andreoli <dave@gurumeditation.it>
2015-12-26 19:41:05 +0100

    Removed broken traileraddict channel (use the new tmdb instead)

--- setup.py
+++ setup.py
@@ -217,7 +217,6 @@ setup (
       'epymc.plugins.uitests': ['*.png'],
       'epymc.plugins.calibrator': ['*.jpg', '*.png'],
       'epymc.plugins.onlinevideo': [ '*.png',
-         'traileraddict/*', #TODO REMOVEME
          'themoviedb/*',
          'youtube/*',
          'vimeo/*',

5890ad9b8be0b779058574c26216db2e0bbffc82
Dave Andreoli <dave@gurumeditation.it>
2016-04-19 20:58:07 +0200

    tvshow: open the databases before populating root page

--- epymc/plugins/tvshows/tvshows.py
+++ epymc/plugins/tvshows/tvshows.py
@@ -351,16 +351,17 @@ class TvShowsModule(EmcModule):
       # if not self._folders:
          #TODO alert the user. and instruct how to add folders
 
-      self._browser.page_add('tvshows://root', _('TV Shows'),
-                             self._styles_for_folders, self.populate_root_page)
-      self._browser.show()
-      mainmenu.hide()
-
       # open movie/idler databases (they are created if not exists)
       if self._tvshows_db is None:
          self._tvshows_db = EmcDatabase('tvshows', TVSHOWS_DB_VERSION)
       if self._idler_db is None:
          self._idler_db = EmcDatabase('tvidlercache', TVSHOWS_DB_VERSION)
+
+      # populate and show root page
+      self._browser.page_add('tvshows://root', _('TV Shows'),
+                             self._styles_for_folders, self.populate_root_page)
+      self._browser.show()
+      mainmenu.hide()
 
       # on idle scan all files (one shot every time the activity start)
       # if not self._scanner and ini.get_bool('movies', 'enable_scanner'):

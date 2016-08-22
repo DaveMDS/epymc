53dda5c84f059a3a882586c9f7b2bd65341757aa
Kai Huuhko <kai.huuhko@gmail.com>
2016-08-22 16:39:40 +0300

    Use env locale
    Fixes date and time display at main screen, maybe others.

--- epymc/gui.py
+++ epymc/gui.py
@@ -21,6 +21,7 @@
 from __future__ import absolute_import, print_function
 
 import os
+import locale
 from datetime import datetime
 
 from efl import evas
@@ -91,6 +92,9 @@ def init():
    global win, layout, theme_file
    global _backdrop_im1, _backdrop_im2, _backdrop_curr
    global _clock_update_timer
+
+   # set locale to user preferred (aka the one set in env) locale
+   locale.setlocale(locale.LC_ALL, '')
 
    # get config values, setting defaults if needed
    theme_name = ini.get('general', 'theme', default_value='default')

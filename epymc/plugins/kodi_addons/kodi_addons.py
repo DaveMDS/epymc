4bd4999ebb0edbad1a10c0c03ea635daab2d640e
Dave Andreoli <dave@gurumeditation.it>
2016-10-23 19:31:08 +0200

    WIP: inital work for kodi repositories
    still will not work on your machine !

--- epymc/plugins/kodi_addons/kodi_addons.py
+++ epymc/plugins/kodi_addons/kodi_addons.py
@@ -49,36 +49,93 @@ from epymc.browser import EmcBrowser, EmcItemClass
 from epymc.gui import EmcDialog
 
 
-def DBG(msg):
-   print('KODI ADDONS: %s' % msg)
-   pass
 
+def DBG(*args):
+   print('KODI ADDONS:', *args)
+   pass
 
 
 
 _mod = None
 
 
+class KodiRepository(object):
+
+   supported_extensions = ('xbmc.python.pluginsource')
+
+   def __init__(self, repo_url):
+      self._addons = []
+      self._base_url = repo_url
+      self._xml_url = repo_url + '/addons.xml'
+      self._xml_cache = os.path.join(utils.user_cache_dir, 'kodirepos', 'asasd', 'addons.xml') # TODO fix asasd !! :)
+      # http://mirrors.kodi.tv/addons/krypton/addons.xml
+
+   def __str__(self):
+      return '<KodiRepo from {}>'.format(self._base_url)
+
+   @property
+   def base_url(self):
+      return self._base_url
+
+   def get_addons(self, done_cb, **kargs):
+      if self._addons:
+         done_cb(self, self._addons, **kargs)
+      self._done_cb = done_cb
+      self._done_cb_kargs = kargs
+      if os.path.exists(self._xml_cache): # TODO check updates (how?)
+         self._parse_xml()
+      else:
+         self._download_xml()
+
+   def _download_xml(self):
+      utils.download_url_async(self._xml_url, dest=self._xml_cache,
+                               complete_cb=self._download_xml_complete_cb,
+                               progress_cb=None)
+      
+
+   def _download_xml_complete_cb(self, dest, status):
+      self._parse_xml()
+
+   def _parse_xml(self):
+      print("PARSE")
+      root = etree.parse(self._xml_cache).getroot()
+      print(root)
+      for addon_el in root.iterchildren():
+         id = addon_el.get('id')
+         if id.startswith(('plugin.video.', 'plugin.audio.', 'plugin.image.')):
+            self._addons.append(KodiAddon(xml_element=addon_el, repo=self))
+
+      self._done_cb(self, self._addons, **self._done_cb_kargs)
+      
+
 
 class KodiAddon(object):
 
-   def __init__(self, path):
+   _main_exe = None # ?????????????????????????
+
+   def __init__(self, path=None, xml_element=None, repo=None):
       print("\n##### ADDON FROM: %s  #####" % path)
       self._path = path
-      self._xml_tree = etree.parse(os.path.join(path, 'addon.xml')) # TODO check errors
+      self._repo = repo
+      if path is not None:
+         document = etree.parse(os.path.join(path, 'addon.xml')) # TODO check errors
+         root = document.getroot()
+      else:
+         root = xml_element
 
       # fetch basic info from the xml file
-      addon = self._xml_tree.getroot()
-      self._id = addon.get('id')
-      self._name = addon.get('name')
-      self._version = addon.get('version')
-      self._author = addon.get('provider-name')
+      
+      self._id = root.get('id')
+      self._name = root.get('name')
+      self._version = root.get('version')
+      self._author = root.get('provider-name')
 
-      for extension in addon.findall('extension'):
+      for extension in root.findall('extension'):
          if extension.get('point') == 'xbmc.python.pluginsource':
             self._main_exe = extension.get('library')
             self._provides = extension.findtext('provides')
 
+      self._xml_tree = root
       self._metadata = None # will be fetched lazily
 
    def __str__(self):
@@ -114,36 +171,40 @@ class KodiAddon(object):
          self._metadata = {}
          self._metadata['screenshots'] = []
          syslang, encoding = locale.getdefaultlocale()
+         if self._path: # local addons
+            assets_base = self._path
+         else: # remote addons
+            assets_base = '{}/{}/'.format(self._repo.base_url, self._id)
          meta = self._xml_tree.find(".//extension[@point='xbmc.addon.metadata']")
          for elem in meta.iterchildren():
             # translatable elements
             if elem.tag in ('summary', 'description', 'disclaimer'):
-               print("TAG", elem.get('lang'), elem.tag)
                lang = elem.get('lang', 'en_GB')
                if lang == syslang or lang == syslang[:2] or lang[:2] == 'en':
                   self._metadata[elem.tag] = elem.text
             # non traslatable elementes
-            elif elem.tag in ('language', 'platform', 'license', 'forum',
-                              'website', 'email', 'source', 'news', 'broken'):
+            elif elem.tag in ('language', 'platform', 'license', 'forum', 
+                              'website', 'email', 'source', 'news', 'broken',
+                              'size', 'nofanart'):
                self._metadata[elem.tag] = elem.text
             # <assets>
             elif elem.tag == 'assets':
                for ass_elem in elem.iterchildren():
                   if ass_elem.tag in ('icon', 'fanart'):
-                     full_path = os.path.join(self._path, ass_elem.text)
+                     full_path = os.path.join(assets_base, ass_elem.text)
                      self._metadata[ass_elem.tag] = full_path
                   elif ass_elem.tag == 'screenshot':
-                     full_path = os.path.join(self._path, ass_elem.text)
+                     full_path = os.path.join(assets_base, ass_elem.text)
                      self._metadata['screenshots'].append(full_path)
             else:
                DBG('METATADA: Unsupported element:', elem.tag)
          # old style assets
          if not self._metadata.get('icon'):
-            icon = os.path.join(self._path, 'icon.png')
-            self._metadata['icon'] = icon if os.path.exists(icon) else None
-         if not self._metadata.get('fanart'):
-            fart = os.path.join(self._path, 'fanart.jpg')
-            self._metadata['fanart'] = fart if os.path.exists(fart) else None
+            icon = os.path.join(assets_base, 'icon.png')
+            self._metadata['icon'] = icon #if os.path.exists(icon) else None
+         if not self._metadata.get('fanart') and self._metadata.get('nofanart') != 'true':
+            fart = os.path.join(assets_base, 'fanart.jpg')
+            self._metadata['fanart'] = fart #if os.path.exists(fart) else None
          
       return self._metadata
 
@@ -277,12 +338,43 @@ class KodiAddon(object):
          pass # TODO ALERT
 
 
+class GetMoreItemClass(EmcItemClass):
+   def item_selected(self, url, mod):
+      mod._browser.page_add('kodi_addons://repos', 'label TODO', None,
+                             mod.populate_repositories_page)
+
+   def label_get(self, url, mod):
+      return _('Get more channels')
+
+   def info_get(self, url, mod):
+      return _('Install, remove or update addons')
+
+   def icon_get(self, url, mod):
+      return 'icon/plus'
+
+class RepoItemClass(EmcItemClass):
+   def item_selected(self, url, repo):
+      _mod._browser.page_add(url, 'label TODO2', None,
+                             _mod.populate_repository_page, repo)
+
+   def label_get(self, url, repo):
+      return 'REPO at {}'.format(repo.base_url)
+
+   # def info_get(self, url, repo):
+      # return _('Install, remove or update addons')
+
+   # def icon_get(self, url, repo):
+      # return 'icon/plus'
+
 class AddonItemClass(EmcItemClass):
    def item_selected(self, url, addon):
       addon.request_page(None)
 
    def label_get(self, url, addon):
       return addon.name.replace('&', '&amp;')
+
+   def label_end_get(self, url, addon):
+      return ' '.join(addon.provides)
 
    def info_get(self, url, addon):
       txt = []
@@ -303,7 +395,6 @@ class AddonItemClass(EmcItemClass):
    def fanart_get(self, url, addon):
       return addon.metadata.get('fanart')
 
-
 class StandardItemClass(EmcItemClass):
    def item_selected(self, url, item_data):
       addon, listitem = item_data
@@ -316,7 +407,9 @@ class StandardItemClass(EmcItemClass):
 
    def icon_get(self, url, item_data):
       addon, listitem = item_data
-      return 'icon/folder' # TODO listitem iconImage or thumbnailImage
+      if listitem.get('isFolder') == True:
+         return 'icon/folder'
+      # TODO listitem iconImage or thumbnailImage
 
    def poster_get(self, url, item_data):
       addon, listitem = item_data
@@ -332,6 +425,7 @@ class StandardItemClass(EmcItemClass):
       except KeyError:
          return None
 
+
 class KodiAddonsModule(EmcModule):
    name = 'kodi_addons'
    label = _('Kodi Addons')
@@ -341,6 +435,7 @@ class KodiAddonsModule(EmcModule):
    _browser = None
    _styles = ('List',)
    _addons = []
+   _repos = []
 
 
    def __init__(self):
@@ -354,6 +449,9 @@ class KodiAddonsModule(EmcModule):
       # ini.add_section('videochannels')
       # ini.get('videochannels', 'autoupdate_ytdl', 'True')
 
+      r = KodiRepository('http://mirrors.kodi.tv/addons/krypton')
+      print(r)
+      self._repos.append(r)
 
       # add an item in the mainmenu
       mainmenu.item_add(self.name, 15, self.label, self.icon, self.mainmenu_cb)
@@ -363,7 +461,7 @@ class KodiAddonsModule(EmcModule):
 
    def __shutdown__(self):
       DBG('Shutdown module')
-      mainmenu.item_del('videochannels')
+      mainmenu.item_del(self.name)
       self._browser.delete()
 
 
@@ -386,10 +484,11 @@ class KodiAddonsModule(EmcModule):
       mainmenu.hide()
 
    def populate_root_page(self, browser, url):
+   
       for addon in sorted(self._addons, key=attrgetter('name')):
-         # self._browser.item_add(AddonItemClass(), None, addon)
          addon.main_item_add(browser)
 
+      browser.item_add(GetMoreItemClass(), 'kodi_addons://manage', self)
    
       # self.current_addon = KodiAddon()
       
@@ -400,6 +499,16 @@ class KodiAddonsModule(EmcModule):
       # self._browser.item_add(DownloadItemClass(), None)
 
 
+   def populate_repositories_page(self, browser, url):
+      for repo in self._repos:
+         browser.item_add(RepoItemClass(), url+'/repo_name', repo) # TODO fix repo_name
 
+   def populate_repository_page(self, browser, url, repo):
+      print("POP33")
+      repo.get_addons(self._repo_get_addons_done)
 
+   def _repo_get_addons_done(self, repo, addons):
+      print("HEYAAA", repo)
+      for addon in sorted(addons, key=attrgetter('name')):
+         self._browser.item_add(AddonItemClass(), 'url', addon)
       
